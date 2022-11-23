9678cb8d5f4d8ee356b7e0f81fcab6c046905871
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-22 19:36:09 -0800

    preemptive css gradients and animations for figma exports

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
+
 
 function Navbar() {
   return (
-    <div>navbar</div>
+    <div className='gpt3_navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={'logo'} alt='logo' />
+        </div>
+      </div>
+      
+    </div>
   )
 }
 

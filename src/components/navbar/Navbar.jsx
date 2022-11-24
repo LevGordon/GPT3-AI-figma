dfc6928663f578a9cb9d4e59c4bb5be5df0cb962
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-23 16:24:16 -0800

    downloaded figma designs and uploaded into assets directory

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
 
+import logo from '../../assets/logo.svg'
+
 
 function Navbar() {
   return (
     <div className='gpt3_navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src={'logo'} alt='logo' />
+          <img src={logo} alt='logo' />
         </div>
       </div>
       

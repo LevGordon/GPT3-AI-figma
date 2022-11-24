c16606c334f8da4464fc3fda1eb87f4124f4605e
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-23 19:04:20 -0800

    header component styled & responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -21,10 +21,12 @@ function Header() {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className='gpt3__header-image'>
-          <img src={Ai} alt="ai futuristic tech"/>
-        </div>
       </div>
+
+      <div className='gpt3__header-image'>
+        <img src={Ai} alt="ai futuristic tech"/>
+      </div>
+
     </div>
   )
 }

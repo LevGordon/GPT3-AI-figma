d1ca24d9a4a35f8e16d7c4153d32cb68b6726d3f
Lev_Gordon <lev.gordonsa@gmail.com>
2022-11-24 12:08:22 -0800

    possibility section jsx, styled and responsive

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './possibility.css'
 
+import possibilityImg from '../../assets/possibility.png'
+
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImg} alt="the possibilities with gpt3" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to get started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to get started</h4>
+      </div>
+      
+    </div>
   )
 }
 
